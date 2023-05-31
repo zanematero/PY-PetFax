2f7934fccdd45da3e084229523c64de84d4e1cf2
Zane Matero <materozane@gmail.com>
2023-05-31 15:06:29 -0700

    used template and blueprint to render json data

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask
 
 def create_app():
-    app = Flask("App")
+    app = Flask("app")
 
     @app.route('/')
     def index():
         return "Hello this is PetFax!"
 
-    @app.route('/pets')
-    def pets_page():
-        return "These are pets up for adoption"
+    # @app.route('/pets', method = ("GET"))
+    # def pets_page():
+    #     return "These are pets up for adoption"
 
+    from .pet import bp
+    app.register_blueprint(bp)
+    
     return app
