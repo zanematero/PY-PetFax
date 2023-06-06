2e46929a1c903540762b2eed667448c72d0e5ab3
Zane Matero <materozane@gmail.com>
2023-06-05 19:32:08 -0700

    created facts get view and post functionality

--- petfax/facts.py
+++ petfax/facts.py
@@ -1,13 +1,22 @@
 from flask import ( Blueprint, render_template, request, redirect )
+from . import models
 
 facts = Blueprint("facts", __name__, url_prefix="/facts")
 
 @facts.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        print(request.form)
+        author = request.form['author']
+        fact = request.form['fact']
+
+        new_fact = models.fact(author=author,fact=fact)
+        models.db.session.add(new_fact)
+        models.db.session.commit()
         return redirect("/facts")
-    return render_template('facts/index.html')
+    
+    results = models.fact.query.all()
+    
+    return render_template('facts/index.html', facts=results)
 
 @facts.route('/new')
 def new():
